3e4fa4665dcd9a9ba3077af7f64eee8ead1f6511
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2024-11-13 02:41:50 -0500

    fix deployment contact from sending bug (#1)
    Co-authored-by: Johnny li <johnnyli@Johnnys-Laptop.local>

--- app/contact/Form.tsx
+++ app/contact/Form.tsx
@@ -57,6 +57,8 @@ const Form = () => {
     const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATEID!;
     const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLICKEY!;
 
+    emailjs.init(publicKey)
+
     const templateParams = {
       from_name : firstNameRef.current?.value,
       from_email: emailRef.current?.value,
