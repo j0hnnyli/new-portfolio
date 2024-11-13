ecac7917a8a0ae659a0dfe84280c8994c47baa58
Johnny li <johnnyli@Johnnys-Laptop.local>
2024-11-13 02:03:49 -0500

    removed unused libraries

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -10,14 +10,6 @@ const page = () => {
     <div className='my-10'>
       <h2 className='uppercase text-5xl tracking-widest text-center font-bold mb-5 text-white'>Skills</h2> 
       <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 items-center justify-center p-2'> 
-        <SkillIcon title='HTML'>
-          <FaHtml5 className='text-8xl text-HTML_color mx-auto'/> 
-        </SkillIcon>
-        
-        <SkillIcon title='CSS'>
-          <FaCss3 className='text-8xl text-CSS_color w-full'/> 
-        </SkillIcon>
-
         <SkillIcon title='Javascript'>
           <IoLogoJavascript className='text-8xl text-Javascript_color w-full'/> 
         </SkillIcon>
@@ -40,6 +32,14 @@ const page = () => {
 
         <SkillIcon title="Tailwind">
           <RiTailwindCssFill className='text-8xl text-Tailwind_color w-full'/> 
+        </SkillIcon>
+
+        <SkillIcon title='HTML'>
+          <FaHtml5 className='text-8xl text-HTML_color mx-auto'/> 
+        </SkillIcon>
+        
+        <SkillIcon title='CSS'>
+          <FaCss3 className='text-8xl text-CSS_color w-full'/> 
         </SkillIcon>
 
         <SkillIcon title="GitHub">
