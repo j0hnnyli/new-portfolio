a22b52afe14ff84798d042d3a193449181d3dec8
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-03-15 21:25:12 -0400

    adjusted gap in project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
   return (
     <div className='text-white'>
       <h2 className='text-5xl font-bold tracking-widest text-center'>My Projects</h2>
-      <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10 p-2">
+      <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-3 my-10 p-2">
         {projects.map((project) => (
           <ProjectCard key={project.title} project={project}/>
         ))}
