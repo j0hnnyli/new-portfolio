0b72ef15edf6142051464b5448fa9c1dab87fd82
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-05-08 02:30:59 -0400

    fixed the overflow bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
       >
         <Navbar />
         <MediaNavbar />
-        <main className="">
+        <main>
           {children}
         </main>
         <Footer />
