49649e80f33024426b064c3c87882e37667ee232
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-05-08 02:34:34 -0400

    Bug : Fixed Overflow Bug  (#37)
    * new portfolio design showcase
    
    * new deisgn for landing page complete
    
    * removed used imports
    
    * design project and contact page
    
    * removed unused import in contact page
    
    * finished redisigning
    
    * added animation for the quote section on the homepage
    
    * updated the tag in the showcase section
    
    * fixed the overflow bug

--- components/homepage/myprocess/MyProcess.tsx
+++ components/homepage/myprocess/MyProcess.tsx
@@ -4,16 +4,14 @@ import MyProcessContent from './MyProcessContent'
 
 const MyProcess = () => {
   return (
-    <section className="py-10 px-5">
-      <div className="flex flex-col md:flex-row justify-between gap-5 max_width">
-        <SectionLabel
-          label="My Process"
-          title="My Creative Flow"
-        />
+    <section className="py-10 px-5 flex flex-col md:flex-row justify-between gap-5 max_width overflow-x-hidden">
+      <SectionLabel
+        label="My Process"
+        title="My Creative Flow"
+      />
 
-        <MyProcessContent />
-      </div>
-  </section>
+      <MyProcessContent />
+    </section>
   )
 }
 
