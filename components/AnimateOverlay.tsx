78357fa6dbbc2c8be3cf6a4eb4e05ba0a40ed139
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-08-18 17:33:43 -0400

    made the top border radius a bit more smooth

--- components/AnimateOverlay.tsx
+++ components/AnimateOverlay.tsx
@@ -15,8 +15,8 @@ const paths = {
 const overlayVariants = {
   initial: {
     top: "100% + 200px",
-    borderTopLeftRadius: "300px",
-    borderTopRightRadius: "300px",
+    borderTopLeftRadius: "200px",
+    borderTopRightRadius: "200px",
   },
   animate: {
     top: '-200px',
@@ -52,36 +52,37 @@ export default function AnimateOverlay() {
   }, [pathname]);
 
   return (
-      <AnimatePresence mode="wait">
-        {showOverlay && (
-          <motion.div
-            key={pathname + "-motion-overlay"}
-            variants={overlayVariants}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            className="fixed top-0 left-0 w-full h-[calc(100vh+200px)] bg-secondary_color z-[100] pointer-events-none flex items-center justify-center"
-          >
-            <div className="flex flex-col items-center justify-center  h-screen w-full mt-[200px]">
-              {pathname === "/juno" && (
-                <div className="bg-secondary_color relative w-36 h-36 rounded-full flex items-center justify-center border-2 border-primary_color">
-                  <Image
-                    src="/juno.png"
-                    alt="juno-avatar"
-                    fill
-                    className="rounded-full object-cover object-top"
-                  />
-                </div>
-              )}
-              <div className="flex items-center gap-5 w-full justify-center">
-                <div className="w-3 h-3 bg-primary_color rounded-full"/>
-                <p className="text-primary_color text-5xl font-bold font-playfair tracking-widest">
-                  {paths[pathname as keyof typeof paths]}
-                </p>
+    <AnimatePresence mode="wait">
+      {showOverlay && (
+        <motion.div
+          key={pathname + "-motion-overlay"}
+          variants={overlayVariants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          className="fixed top-0 left-0 w-full h-[calc(100vh+200px)] bg-secondary_color z-[100] pointer-events-none flex items-center justify-center"
+        >
+          <div className="flex flex-col items-center justify-center  h-screen w-full mt-[200px]">
+            {pathname === "/juno" && (
+              <div className="bg-secondary_color relative w-36 h-36 rounded-full flex items-center justify-center border-2 border-primary_color">
+                <Image
+                  src="/juno.png"
+                  alt="juno-avatar"
+                  fill
+                  className="rounded-full object-cover object-top"
+                />
               </div>
+            )}
+
+            <div className="flex items-center gap-5 w-full justify-center">
+              <div className="w-3 h-3 bg-primary_color rounded-full"/>
+              <p className="text-primary_color text-5xl font-bold font-playfair tracking-widest">
+                {paths[pathname as keyof typeof paths]}
+              </p>
             </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
