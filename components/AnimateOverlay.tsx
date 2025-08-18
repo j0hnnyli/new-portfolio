9bdc77e4601f4565802c311b3db3b98e5176d484
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-08-18 02:11:14 -0400

    fixed page transition bug in production

---
+++ components/AnimateOverlay.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { AnimatePresence, motion } from "framer-motion";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const paths = {
+  "/": "Home",
+  "/projects": "Projects",
+  "/contact": "Contact",
+  "/juno": "Juno",
+};
+
+const overlayVariants = {
+  initial: {
+    top: "100% + 200px",
+    borderTopLeftRadius: "90%",
+    borderTopRightRadius: "90%",
+  },
+  animate: {
+    top: '-200px',
+    borderTopLeftRadius: "0",
+    borderTopRightRadius: "0",
+    transition: {
+      duration: 0.8,
+      ease: [0.22, 1, 0.36, 1],
+    },
+  },
+  exit: {
+    top: "-150%",
+    borderBottomLeftRadius: "100%",
+    borderBottomRightRadius: "100%",
+    transition: {
+      duration: 0.8,
+      ease: [0.22, 1, 0.36, 1],
+    },
+  },
+};
+
+export default function AnimateOverlay() {
+  const pathname = usePathname();
+  const [showOverlay, setShowOverlay] = useState(false);
+  
+  useEffect(() => {
+    setShowOverlay(true);
+    const timer = setTimeout(() => {
+      setShowOverlay(false);
+    }, 800);
+    
+    return () => clearTimeout(timer);
+  }, [pathname]);
+
+  return (
+      <AnimatePresence mode="wait">
+        {showOverlay && (
+          <motion.div
+            key={pathname + "-motion-overlay"}
+            variants={overlayVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="fixed top-0 left-0 w-full h-[calc(100vh+200px)] bg-secondary_color z-[100] pointer-events-none flex items-center justify-center"
+          >
+            <div className="flex flex-col items-center justify-center  h-screen w-full mt-[200px]">
+              {pathname === "/juno" && (
+                <div className="bg-secondary_color relative w-36 h-36 rounded-full flex items-center justify-center border-2 border-primary_color">
+                  <Image
+                    src="/juno.png"
+                    alt="juno-avatar"
+                    fill
+                    className="rounded-full object-cover object-top"
+                  />
+                </div>
+              )}
+              <div className="flex items-center gap-5 w-full justify-center">
+                <div className="w-3 h-3 bg-primary_color rounded-full"/>
+                <p className="text-primary_color text-5xl font-bold font-playfair tracking-widest">
+                  {paths[pathname as keyof typeof paths]}
+                </p>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+  );
+}
