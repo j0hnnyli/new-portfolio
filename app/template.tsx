b7c0402dbc4f4f4ded3fdc2fd274608098910f1c
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-08-18 01:56:52 -0400

    developed a page transition overlay

---
+++ app/template.tsx
@@ -0,0 +1,118 @@
+'use client'
+
+import { AnimatePresence, motion } from "framer-motion";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useEffect, useRef, useState, ReactNode } from "react";
+
+const paths = {
+  "/": "Home",
+  "/projects": "Projects",
+  "/contact": "Contact",
+  "/juno": "Juno",
+};
+
+const overlayVariants = {
+  initial: {
+    top: "100% + 200px",
+    borderTopLeftRadius: "90%",
+    borderTopRightRadius: "90%",
+  },
+  animate: {
+    top: '-200px',
+    borderTopLeftRadius: "0",
+    borderTopRightRadius: "0",
+    transition: {
+      duration: 0.8,
+      ease: [0.22, 1, 0.36, 1],
+    },
+  },
+  exit: {
+    top: "-150%",
+    borderBottomLeftRadius: "100%",
+    borderBottomRightRadius: "100%",
+    transition: {
+      duration: 0.8,
+      ease: [0.22, 1, 0.36, 1],
+    },
+  },
+};
+
+const AnimatePageTransition = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname();
+  const [showOverlay, setShowOverlay] = useState(false);
+  const isFirstRender = useRef(true); 
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false; 
+      return;
+    }
+
+    setShowOverlay(true);
+    const timer = setTimeout(() => {
+      setShowOverlay(false);
+    }, 800);
+    
+    return () => clearTimeout(timer);
+  }, [pathname]);
+
+  const pageVariants = {
+    initial: { opacity: 0, y: 40 },
+    animate: { 
+      opacity: 1, 
+      y: 0,
+      transition : { 
+        duration: 0.6, 
+        delay: isFirstRender ? 0 : .8,
+        ease: 'easeIn' 
+      },
+    },
+  }
+
+
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        {showOverlay && (
+          <motion.div
+            key={pathname + "-motion-overlay"}
+            variants={overlayVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="fixed top-0 left-0 w-full h-[calc(100vh+200px)] bg-secondary_color z-[100] pointer-events-none flex items-center justify-center"
+          >
+            <div className="flex flex-col items-center justify-center  h-screen w-full mt-[200px]">
+              {pathname === "/juno" && (
+                <div className="bg-secondary_color relative w-36 h-36 rounded-full flex items-center justify-center border-2 border-primary_color">
+                  <Image
+                    src="/juno.png"
+                    alt="juno-avatar"
+                    fill
+                    className="rounded-full object-cover object-top"
+                  />
+                </div>
+              )}
+              <div className="flex items-center gap-5 w-full justify-center">
+                <div className="w-3 h-3 bg-primary_color rounded-full"/>
+                <p className="text-primary_color text-5xl font-bold font-playfair tracking-widest">
+                  {paths[pathname as keyof typeof paths]}
+                </p>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <motion.div
+        variants={pageVariants}
+        initial="initial"
+        animate="animate"
+      >
+        {children}
+      </motion.div>
+    </>
+  )
+}
+
+export default AnimatePageTransition
--- components/MediaNavbar.tsx
+++ components/MediaNavbar.tsx
@@ -5,52 +5,46 @@ import {
   SheetDescription,
   SheetTitle,
   SheetTrigger,
-  SheetClose
-} from "@/components/ui/sheet"
+  SheetClose,
+} from "@/components/ui/sheet";
 import Link from "next/link";
 import ShowcaseAnimation from "./homepage/showcase/ShowcaseAnimation";
 import ChatBoxLink from "./ChatBoxLink";
 
 const MediaNavbar = () => {
-
   return (
-    <div className='fixed md:hidden top-0 w-full p-5 flex items-center justify-between h-20 z-50 bg-primary_color'>
-      <Link 
-        href='/'
-        className='font-bold text-lg'
-      >
+    <div className="fixed md:hidden top-0 w-full p-5 flex items-center justify-between h-20 z-50 bg-primary_color">
+      <Link href="/" className="font-bold text-lg">
         <h2>Johnny</h2>
         <h2>Li _ </h2>
       </Link>
 
       <Sheet>
         <SheetTrigger>
-          <RiMenu3Fill className='text-2xl'/> 
+          <RiMenu3Fill className="text-2xl" />
         </SheetTrigger>
-        <SheetContent className='bg-primary_color'>
+        <SheetContent className="bg-primary_color">
           <SheetDescription></SheetDescription>
           <div className="flex items-center justify-center">
             <ChatBoxLink isMobile={true} />
           </div>
           <ShowcaseAnimation />
 
-          <SheetTitle className='font-bold text-xl mt-5 text-center'>
-            Johnny Li _ 
+          <SheetTitle className="font-bold text-xl mt-5 text-center">
+            Johnny Li _
           </SheetTitle>
-          
+
           <div className="mt-5 flex flex-col items-center justify-center gap-3 text-xl">
-            <Link href='/' >
+            <Link href="/">
               <SheetClose>Home</SheetClose>
             </Link>
-            <Link href='/projects'>
+            <Link href="/projects">
               <SheetClose>Projects</SheetClose>
             </Link>
-            <Link href='/contact'>
+            <Link href="/contact">
               <SheetClose>Contact</SheetClose>
             </Link>
-            
           </div>
-
         </SheetContent>
       </Sheet>
     </div>
