f662a0deeb41d8fb7a22b6d9c2153776cdff4e09
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-09-03 14:47:26 -0400

    developed another approach to solve production bug for transition from project page to dyanmic route

--- app/projects/[id]/FeatureSection.tsx
+++ app/projects/[id]/FeatureSection.tsx
@@ -43,7 +43,6 @@ export default function FeatureSection( { features } : FeatureSectionProps) {
           ))}
         </motion.div>  
       </div>
-
     </div>
   );
 }
--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,21 +1,24 @@
+'use client'
+
+import { motion } from 'framer-motion';
+import { fadeIn, staggerContainer } from '@/lib/motions';
+import { ArrowLeft } from 'lucide-react';
+import { NavLinkWrapper } from '@/components/NavLink';
+import { useParams } from 'next/navigation';
 import projects from '@/lib/content/projects';
-import React from 'react';
 import Image from 'next/image';
 import Footer from '@/components/Footer';
 import SectionLabel from '@/components/SectionLabel';
 import ProcessSection from './ProcessSection';
 import DetailSection from './DetailSection';
-import FeatureSection from './FeatureSection';
-import { ArrowLeft } from 'lucide-react';
-import { NavLinkWrapper } from '@/components/NavLink';
+import dynamic from 'next/dynamic';
+import responsiveAnimationData from '@/assets/responsiveanimation.json';
 
-type ProjectPageProps = {
-  params: Promise<{id: string}>
-}
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
 
-export default async function ProjectPage({ params }: ProjectPageProps) {
-  const id = (await params).id
-  const project = projects.find((project) => project.id === Number(id))
+export default function ProjectPage() {
+  const params = useParams();
+  const project = projects.find((project) => project.id === Number(params.id))
 
   if(!project) return;
 
@@ -52,7 +55,36 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
         </div>
 
         <section className='bg-secondary_color my-10'>
-          <FeatureSection features={project.features}/>
+          <div className='max_width p-5 flex flex-col md:flex-row items-center justify-between'>
+            <div className='w-3/4 md:w-full overflow-hidden'>
+              <Lottie animationData={responsiveAnimationData} className=''/>
+            </div>
+
+            <div className='w-full'>
+              <h2 className='text-primary_color font-playfair text-4xl text-center'>
+                Features
+              </h2>
+
+              <motion.div 
+                variants={staggerContainer(0.2, 0.2)}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once : true, amount: 0.2}}
+                className='grid grid-cols-1 md:grid-cols-2 gap-5 text-primary_color font-playfair my-5'
+              >
+                {project.features.map((feat) => (
+                  <motion.div
+                    key={feat}
+                    variants={fadeIn('up', 'spring')}
+                    className='flex items-center gap-5 p-2'
+                  >
+                    <div className='w-3 h-3 bg-third_color rounded-full animate-pulse flex-shrink-0'/>
+                    <p>{feat}</p>
+                  </motion.div>
+                ))}
+              </motion.div>  
+            </div>
+          </div>
         </section>
 
         <section className='flex flex-col md:flex-row justify-between gap-5 overflow-hidden max_width px-5'>
