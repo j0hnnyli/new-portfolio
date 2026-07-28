355d1513ade4c01e5141ea25d34c4f3a9d86b4d0
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2026-07-28 19:53:18 -0400

    Revamped completed

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
+import { twMerge } from 'tailwind-merge';
 
 type Props = {
-  mode : 'light' | 'dark'
+  mode : 'light' | 'dark';
+  className?: string;
 }
 
-const Grid = ({ mode }: Props) => {
+const Grid = ({ mode, className }: Props) => {
   return (
     <>
       {mode === 'dark' ? (
-          <div className="absolute inset-y-0 left-1/2 w-screen -translate-x-1/2 
-            bg-[linear-gradient(to_right,rgba(255,255,255,0.05)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.05)_1px,transparent_1px)] bg-[size:80px_80px] pointer-events-none z-0">
+          <div className={twMerge(`absolute inset-y-0 left-1/2 w-screen -translate-x-1/2 
+            bg-[linear-gradient(to_right,rgba(255,255,255,0.05)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.05)_1px,transparent_1px)] bg-[size:80px_80px] pointer-events-none z-0`, className)}>
           </div>
         ) : (
-          <div className="absolute inset-y-0 left-1/2 w-screen -translate-x-1/2 
+          <div className={twMerge(`absolute inset-y-0 left-1/2 w-screen -translate-x-1/2 
             bg-[linear-gradient(to_right,#e5e7eb_1px,transparent_1px),linear-gradient(to_bottom,#e5e7eb_1px,transparent_1px)] 
             bg-[size:80px_80px] 
             pointer-events-none 
-            z-0">
+            z-0`, className)}>
           </div>
         )
       }
